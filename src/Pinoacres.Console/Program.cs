4b416ec5102e66edbcf86ce88223ff37f861f378
blongacre22 <longacre.bruce@gmail.com>
2016-07-07 09:57:01 -0700

    Re-created Console project.

---
+++ src/Pinoacres.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pinoacres.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Console app MAIN program.");
+        }
+    }
+}
