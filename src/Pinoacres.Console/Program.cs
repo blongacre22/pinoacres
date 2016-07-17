b57d1c63459715d8bb985ec501bcf6bb42700808
blongacre22 <longacre.bruce@gmail.com>
2016-07-17 15:17:35 -0700

    Added test MLB Pinger code to Console project.

--- src/Pinoacres.Console/Program.cs
+++ src/Pinoacres.Console/Program.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Net.Http;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
 namespace Pinoacres.Console
 {
     public class Program
@@ -11,6 +19,19 @@ namespace Pinoacres.Console
         {
             System.Console.WriteLine("Console app MAIN program.");
             Lib.TestClass.TestMethod();
+
+            string url = "http://www.mlbextrabases.com/pubajaxws/bamrest/product/v-1.1?catCode=BOA_TICKETS-sfn-08-2016&lang=en&view=game_cal";
+            //var contents = new System.Net.WebClient().DownloadString(url);
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            var responseTask = request.GetResponseAsync();
+            var response = responseTask.Result;
+            var stream = response.GetResponseStream();
+            var sr = new System.IO.StreamReader(stream);
+            var json = sr.ReadToEnd();
+
+            json = json.Replace("var products = ", string.Empty);
+
+            var objects = JsonConvert.DeserializeObject(json);
         }
     }
 }
